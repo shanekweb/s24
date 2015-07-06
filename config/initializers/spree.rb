3f698b7237b1fd111db7aa2000fe349a03b0dfcb
Shane Kelly <shanekweb@icloud.com>
2015-07-06 16:25:03 +0100

    allow back order false track inv true

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,6 +10,8 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
+	config.track_inventory_levels = true
+    	config.allow_backorder_shipping = false
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
