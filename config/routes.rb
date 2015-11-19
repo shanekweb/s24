1692c0d6a4d54279e78a9c61b51900ef9c8ed648
Shane Kelly <shanekweb@icloud.com>
2015-11-19 23:33:07 +0000

    root

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,47 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, :at => '/home'
+  mount Spree::Core::Engine, :at => '/'
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   get '/home', :to => 'spree/home#home', :as => :home
   # You can have the root of your site routed with "root"
+Spree::Core::Engine.add_routes do
 
+  root :to => 'home#index'
+
+  resources :products, :only => [:index, :show]
+
+  get '/locale/set', :to => 'locale#set'
+
+  # non-restful checkout stuff
+  patch '/checkout/update/:state', :to => 'checkout#update', :as => :update_checkout
+  get '/checkout/:state', :to => 'checkout#edit', :as => :checkout_state
+  get '/checkout', :to => 'checkout#edit' , :as => :checkout
+
+  populate_redirect = redirect do |params, request|
+    request.flash[:error] = Spree.t(:populate_get_error)
+    request.referer || '/cart'
+  end
+
+  get '/orders/populate', :to => populate_redirect
+
+  resources :orders, :except => [:index, :new, :create, :destroy] do
+    post :populate, :on => :collection
+  end
+
+  get '/cart', :to => 'orders#edit', :as => :cart
+  patch '/cart', :to => 'orders#update', :as => :update_cart
+  put '/cart/empty', :to => 'orders#empty', :as => :empty_cart
+
+  # route globbing for pretty nested taxon and product paths
+  get '/t/*id', :to => 'taxons#show', :as => :nested_taxons
+
+  get '/unauthorized', :to => 'home#unauthorized', :as => :unauthorized
+  get '/content/cvv', :to => 'content#cvv', :as => :cvv
+  get '/content/*path', :to => 'content#show', :as => :content
+  get '/cart_link', :to => 'store#cart_link', :as => :cart_link
+end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
