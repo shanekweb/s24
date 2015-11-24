cc0eb4c58cb135fef5a8f0a5a7a46ea05c3e76bb
Shane Kelly <shanekweb@icloud.com>
2015-11-24 16:48:06 +0000

    removed mig

--- db/migrate/20151124164334_remove_cols_from_spree_products.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColsFromSpreeProducts < ActiveRecord::Migration
-  def change
-  	remove_column :spree_products, :imagePath2
-  	remove_column :spree_products, :pp
-  	remove_column :spree_products, :hello
-  end
-end
